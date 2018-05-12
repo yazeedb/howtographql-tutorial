97cf8bc0bfc1471452f566660540c52607e5021b
yazeedb <ybzadough@gmail.com>
2018-05-12 15:00:41 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const { GraphQLServer } = require('graphql-yoga');
+const links = require('./links.json');
+
+const resolvers = {
+  Query: {
+    info: () => 'This is the Hackernews Clone API',
+    feed: () => links,
+    link: (root, { id }) => links.find((link) => link.id === id)
+  },
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${links.length}`,
+        description: args.description,
+        url: args.url
+      }
+
+      links.push(link);
+
+      return link;
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers
+})
+
+server.start(() => console.log('Running on port 4000'))
+// server.start(console.log)
