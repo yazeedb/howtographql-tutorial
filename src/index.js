ab03fe23265232f1bf84c816885c4cffbd849b47
yazeedb <ybzadough@gmail.com>
2018-05-13 16:38:40 -0400

    add spacing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const { GraphQLServer } = require('graphql-yoga');
 let links = require('./links.json');
+
 const resolvers = {
   Query: {
     links: () => links
   }
 };
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers
 });
+
 server.start(() => console.log('Running on port 4000'));
